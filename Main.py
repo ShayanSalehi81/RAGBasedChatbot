6cfab9f866ab5708d583e784a72468856cec57dd
Shayan Salehi <79784127+ShayanSalehi81@users.noreply.github.com>
2024-10-20 10:30:20 +0330

    Change The Main File

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
 from SpellingMistakeCorrector.SpellingMistakesCorrector import SpellingMistakeCorrector
 from SwearWordDetector.SwearWordDetector import SwearWordDetector
+from ChatBot.ChatBot import Chat
 
 
 class Chatbot:
     def __init__(self) -> None:
         self.swear_detector = SwearWordDetector('SwearWordDetector/swear_words.json')
         self.spelling_corrector = SpellingMistakeCorrector('SpellingMistakeCorrector/frequency_combined.csv', threshold=0.2)
+        self.chat_bot = Chat()
 
     def chat(self, query) -> str:
         spellCorrectedQuery = self.spelling_corrector.correct_spelling(query)
         censoredQuery = self.swear_detector.filter_words(spellCorrectedQuery)
+        
         return censoredQuery
 
 
